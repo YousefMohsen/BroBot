06c1eb74074b13689594ecfea7e3536260b498f0
Combii <davidstovle@gmail.com>
2019-06-11 11:45:28 +0200

    Deleting comments

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ from smbus import SMBus
 
 sound = Sound()
 sound.beep()
-#sound.speak("Boris shut up")
 gyro = GyroSensor(INPUT_2)
 motors = MoveTank(OUTPUT_D, OUTPUT_A)
 steering_drive = MoveSteering(OUTPUT_D, OUTPUT_A)
@@ -22,20 +21,12 @@ us = UltrasonicSensor(INPUT_3)
 infraSensor = InfraredSensor(INPUT_4)
 infraSensor.mode = 'IR-PROX'
 
-# tank_pair = MoveTank(OUTPUT_D, OUTPUT_A)
-#init pixy 
-# get a Port object to control the input port
+
 in1 = LegoPort(address=INPUT_1)
-# force the port into i2c-other mode so that the default driver is not automatically loaded
 in1.mode = 'other-i2c'
-
-# might need a short delay here (e.g. time.sleep(0.5)) to wait for the I2C port to be setup
-# note: this delay should only be needed in ev3dev-jessie (4.4.x kernel), not ev3dev-stretch (4.9.x kernel)
-
 
 bus = SMBus(3)  # bus number is input port number + 2
 I2C_ADDRESS = 0x01  # the default I2C address of the sensor
-
 
 def resetGyroAngle():
     motors.off()
@@ -43,89 +34,61 @@ def resetGyroAngle():
     gyro.mode = 'GYRO-ANG'
 
 def drive(speed):
-    print("messi")
     motors.on(SpeedPercent(-speed), SpeedPercent(-speed))
 
 
 def stopMotor(motor):
     motor.off()
 
-# move forward for 2 seconds
-#tank_drive = MoveTank(OUTPUT_D, OUTPUT_A)
-#tank_drive.on_for_seconds(SpeedPercent(-20), SpeedPercent(-20), 1)
-
 
 def turnRightByDegrees(degrees):
-    print("moveForwardGyro")
     resetGyroAngle()
 
     turnSpeed = -20
-    # Connect gyro sensor.
-
-    # Start the left motor with speed 40% to initiate a medium turn right.
+    
     motors.on(left_speed=turnSpeed, right_speed=-turnSpeed)
 
-    # Wait until the gyro sensor detects that the robot has turned
-    # (at least) 90 deg in the positive direction (to the right)
     gyro.wait_until_angle_changed_by(degrees)
     motors.off()
-
-    # Robot moves straight ahead with speed 50% until the wheels
-    # have turned through one rotation
-    #tank_pair.on_for_rotations(left_speed=50, right_speed=50, rotations=1)
 
 
 def turnLeftByDegrees(degrees):
-    print("moveForwardGyro")
     resetGyroAngle()
     turnSpeed = -20
-    # Connect gyro sensor.
 
-    # Start the left motor with speed 40% to initiate a medium turn right.
     motors.on(left_speed=-turnSpeed, right_speed=turnSpeed)
 
-    # Wait until the gyro sensor detects that the robot has turned
-    # (at least) 90 deg in the positive direction (to the right)
     gyro.wait_until_angle_changed_by(degrees)
     motors.off()
 
-    # Robot moves straight ahead with speed 50% until the wheels
-    # have turned through one rotation
-    #tank_pair.on_for_rotations(left_speed=50, right_speed=50, rotations=1)
-
-
 def driveStraightGyro(power):
     resetGyroAngle()
-    #error = (gyro.angle / 360) * 100
     error =  -1 * gyro.angle
-    #print("error ",error)
-    print("gyro.angle ",gyro.angle)
     time.sleep(1)
-    #if(error == 0):
-    #    drive(power)
-    #else:
     steering_drive.on(error, -power)
     
 
 
-def turnBack(left,turnDegrees):
+def turnBack(left,turnDegrees, tracks):
     motors.off()
     if(left):
         turnLeftByDegrees(turnDegrees)
-        motors.on_for_rotations(10, 10, -0.7)
+        motors.on_for_rotations(10, 10, -0.85)
         turnLeftByDegrees(turnDegrees)
     else:
         turnRightByDegrees(turnDegrees)
-        motors.on_for_rotations(10, 10, -0.7)
+        if(tracks == 5):
+            motors.on_for_rotations(10, 10, -0.65)
+        else:
+            motors.on_for_rotations(10, 10, -0.85)
         turnRightByDegrees(turnDegrees)
 
-    motors.on_for_rotations(left_speed=-25, right_speed=-15, rotations=0.5)
-    if(turnDegrees==90):
+
+   # motors.on_for_rotations(left_speed=-25, right_speed=-15, rotations=0.5)
+    """if(turnDegrees==90):
         return 85
     else:
-       return 90
-
-
+       return 90"""
 
 
 def lookForObstacle(signatureToFind,callback):
@@ -145,37 +108,10 @@ def lookForObstacle(signatureToFind,callback):
 
 
 
-
-
-def main():
-    turnDegrees = 90
-    turnRight = True
-    resetGyroAngle()
-    lookForObstacle()
-    while True:
-        while(us.distance_centimeters > 20):
-           # driveStraightGyro(30)
-            drive(30)
-
-        if(turnRight):
-          turnDegrees =  turnBack(False, turnDegrees)
-          turnRight = False
-        else:
-           turnDegrees = turnBack(True, turnDegrees)
-           turnRight = True
-
-
-
 def obstacleFound(x, y, width, height):
         motors.off()
         motors.on_for_rotations(10,10,0.5) #move 2 rotations back
-        #turnLeftByDegrees(90) #turn left
-        #motors.on_for_rotations(-10,-10,1) #move 6 rotations back
-        #turnRightByDegrees(90) #turn right
-        #motors.on_for_rotations(-10,-10,2.9) #move 6 rotations back
-        #turnRightByDegrees(90) #turn right
-        #motors.on_for_rotations(-10,-10,0.5) #move 6 rotations back
-        #turnLeftByDegrees(90) #turn right
+ 
         
         turnLeftByDegrees(50) #turn left
         motors.on_for_rotations(10,10,-2) 
@@ -184,75 +120,38 @@ def obstacleFound(x, y, width, height):
         turnLeftByDegrees(50) #turn left
 
 
-
-
-
-
-
-      # motors.on_for_rotations(-10,-10,2) #move 6 rotations back
-
-             
-
-#while True:
- #    lookForObstacle(1,obstacleFound)
-#obstacleFound(1,2,3,4)
-
-
-#def findGoal():
-    #find
-
-#main()
-#turnBack(True,90)
-#turnRightByDegrees(90)
-#turnLeftByDegrees(90)
-#turnRightByDegrees(90)
-#turnLeftByDegrees(90)
-#while True:
-    """cm = (infraSensor.proximity/100)*70
-    print("cm:",cm)
-   # print("cm shit",us.distance_centimeters)
-    time.sleep(1)"""
- #   drive(30)
-
-#driveAlongWall(10)
-#steering_drive.on_for_rotations(-20, -10, 0.5)
-
 def driveAlongWall(dist):
     #dist = 20
     #while(True): 
         while(25 < (infraSensor.proximity/100)*70):
             print("us.distance_centimeters",us.distance_centimeters)
+            time.sleep(1)
             if(us.distance_centimeters < dist):
-                motors.on(-20, -15)
+                motors.on(-30, -25)
             else:
                 if(us.distance_centimeters > dist):
-                    motors.on(-15, -20)
+                    motors.on(-25, -30)
                 else:
-                    motors.on(-20, -20)
+                    motors.on(-30, -30)
 
 def sweep():
-    tracksDistance = [8,80,45,45,80,8]
+    tracksDistance = [8,80,55,55,80,8]
+
     turnRight = True
     for index, track in enumerate(tracksDistance, start=0):
        # print("track",track)
         driveAlongWall(track)
         if(len(tracksDistance)-1!=index ):
             if(turnRight):
-                turnBack(False, 90)
+                turnBack(False, 90, index+1)
                 turnRight = False
             else:
-                turnBack(True, 90)
+                turnBack(True, 90, index+1)
                 turnRight = True
         else: #find goal
             motors.off()
             sound.beep()
         
-
-
-
-#while True:
-    #motors.on(left_speed=-40, right_speed=-10)
-  # print("us.distance_centimeters", us.distance_centimeters)
- #  time.sleep(2)
+        print("Counter", index+1)
    
 sweep()
